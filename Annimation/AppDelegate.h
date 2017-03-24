7a2358f7c89f665cf202d665f3d04d8ea0576ffd
caoxiaolei <xiaolei.cao@aayongche.com>
2017-03-24 15:27:56 +0800

    zz

---
+++ Annimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Annimation
+//
+//  Created by 晓磊 on 17/1/11.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Annimation/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Annimation
+//
+//  Created by 晓磊 on 17/1/11.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
